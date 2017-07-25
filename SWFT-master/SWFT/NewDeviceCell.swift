5141825739417050fa0d3af0268de06dd53c5884
Pat <pat@mazesys.com>
2017-07-25 15:50:39 +0100

    Updated methods for multicast
    Updated methods for multicast delegate in BluetoothManager

---
+++ SWFT-master/SWFT/NewDeviceCell.swift
@@ -0,0 +1,16 @@
+//
+//  NewDeviceCell.swift
+//  SWFT
+//
+//  Created by Pat Dynek on 04/07/2017.
+//  Copyright © 2017 Qualcomm Technology Limited Ltd. All rights reserved.
+//
+
+import UIKit
+
+class NewDeviceCell: UITableViewCell {
+    
+    @IBOutlet weak var deviceNameLabel: UILabel!
+    @IBOutlet weak var deviceUUIDLabel: UILabel!
+    
+}
